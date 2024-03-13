de3e888b2f79fa3f1db2d615f47e8b72b36afb13
Nigel <nigel@UNSC-Halsey.Nigel>
2024-03-13 17:15:29 +0200

    create welcomeScreen function

---
+++ numberSystemConverter.py
@@ -0,0 +1,16 @@
+def welcomeScreen ():
+
+    print ('---------------------------------------------------------')
+    print ('>>> Welcome to the Number System Converter Program 😊 <<<')
+    print ('---------------------------------------------------------')
+
+    print(">> Select Conversion Type: ")
+    print("> 1. Binary Conversion")
+    print("> 2. Decimal Conversion")
+    print("> 3. Octal Conversion")
+    print("> 4. Hexadecimal Conversion")
+    print("> 5. Exit the Program \n")
+
+    choice = int ( input ("Enter the number & Hit ENTER: "))
+
+welcomeScreen()
